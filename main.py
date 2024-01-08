860a0bebcb0876d1d4fd1c6ef9d2d972aab31e92
Hussein <118964436+SS7SS@users.noreply.github.com>
2024-01-08 11:39:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 import uvicorn
 from telethon.sessions import StringSession
 
@@ -8,12 +8,20 @@ app = FastAPI()
 api_id = 23398930
 api_hash = 'bd3e85a7aae40566f2fa8804d200d6d0'
 
-@app.get("/api/{sess}")
-async def send_message(sess: str):
+
+@app.get("/api/{sess}/1/{user}/{message}/")
+async def send_message(sess: str, user: int, message: str):  # إضافة user و message كمتغيرات للدالة
+
     async with TelegramClient(StringSession(sess), api_id, api_hash) as client:
-        receiver = await client.get_input_entity('me')
-        await client.send_message(receiver, 'مرحبا')
+        await client.start()
+
+
+        await client.send_message(user, message)
+
+        await client.disconnect()
+
     return {"message": "Message sent"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
