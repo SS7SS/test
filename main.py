c558cee6f9853ac4381079fae13dad3d7d73660b
Hussein <118964436+SS7SS@users.noreply.github.com>
2024-01-08 11:53:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,9 @@ api_hash = 'bd3e85a7aae40566f2fa8804d200d6d0'
 @app.get("/api/{sess}/1/{user}/{message}/")
 async def send_message(sess: str, user: str, message: str):
 
-    async with TelegramClient(StringSession(sess), api_id, api_hash) as client:
-        await client.start()
-
-
+    async with TelegramClient(StringSession(sess), api_id, api_hash) as client:        
         await client.send_message(user, message)
-
-        await client.disconnect()
-
     return {"message": "Message sent"}
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
